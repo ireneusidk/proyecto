842352df31102912055bda7efe02e6ceda6b9431
ireneusidk <k-my99@hotmail.com.ar>
2024-11-06 17:23:45 -0300

    7

--- front/vite.config.js
+++ front/vite.config.js
@@ -5,12 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://proyecto-m9kz.onrender.com', // Reemplaza esta URL con la URL de tu backend en Render
-        changeOrigin: true,
-        secure: false, // Si tu backend usa HTTPS, cambia esto a true
-      },
-    },
-  },
+    port: process.env.PORT || 4173,  // Usa el puerto asignado por Render o un valor predeterminado local
+    host: '0.0.0.0'  // Asegúrate de exponer el puerto para que Render lo pueda acceder
+  }
 })
