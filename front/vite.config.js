666f608e947b78d598057041164ad479e3e880af
Ireneus_idk <k-my99@hotmail.com.ar>
2024-11-11 16:30:39 -0300

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 4173, // Usa el puerto proporcionado por Render o el 4173 para desarrollo local
+    host: '0.0.0.0',  // Escucha en 0.0.0.0 en lugar de localhost
+    port: process.env.PORT || 4173,  // Usa el puerto proporcionado por Render o el 4173 para desarrollo local
     proxy: {
       '/api': {
         target: process.env.VITE_BACKEND_URL || 'https://proyecto-m9kz.onrender.com',
