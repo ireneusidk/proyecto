8c1c88c680553691277de89aef5f2add6afa0f6e
Ireneus_idk <k-my99@hotmail.com.ar>
2024-11-11 17:08:17 -0300

    Update api.jsx

--- front/src/componentes/api/api.jsx
+++ front/src/componentes/api/api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://proyecto-1-1qny.onrender.com/api'
+  baseURL: 'https://proyecto-1-1qny.onrender.com/api',
+  withCredentials: true 
 })
 
 export default api;
